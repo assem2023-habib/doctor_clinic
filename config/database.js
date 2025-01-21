35dad2361b4c772683570a4d342c555bbabcea41
assem2023-habib <144098648+assem2023-habib@users.noreply.github.com>
2025-01-21 12:56:57 +0300

    make updates on database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('beauty_center', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+// const sequelize = new Sequelize('beauty_center', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  });
 
 sequelize.authenticate().then(() => {
     console.log('Connection to MySQL has been established successfully.');
